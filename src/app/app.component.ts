26605211b7882017020fdd33bf02ac3a05e18e44
mmsalim <happymarja@gmail.com>
2025-03-08 22:36:45 +0530

    installed web component lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WebComponentsComponent, WebComponentsService } from 'web-components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WebComponentsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  private webComponentsService = inject(WebComponentsService);
+
+  message: string = '';
+  ngOnInit(): void {
+    this.message = this.webComponentsService.getHello();
+  }
   title = 'demo';
 }
